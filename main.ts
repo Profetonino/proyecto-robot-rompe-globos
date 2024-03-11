ff90af2503fe30f7a14cc8be1502b71bcbe63dd4
J. Antonio Pérez <52037760+Profetonino@users.noreply.github.com>
2024-03-11 19:01:52 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
+/**
+ * PLACA CONTROL SERVOS
+ */
+// PLACA CONTROL SERVOS
 radio.onReceivedNumber(function (receivedNumber) {
     valor_Servo = Math.map(receivedNumber, -1023, 1023, 5, 170)
     servos.P0.setAngle(valor_Servo)
 })
+// CODIGO PARA LA PLACA DE MANDO
 input.onButtonPressed(Button.A, function () {
     radio.sendString("ataca")
 })
+// PLACA CONTROL SERVOS
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "ataca") {
         servos.P1.setAngle(90)
@@ -17,14 +23,20 @@ radio.onReceivedString(function (receivedString) {
         servos.P2.setAngle(0)
     }
 })
+/**
+ * CÓDIGO PARA PLACA QUE HACE DE MANDO
+ */
+// CODIGO PARA LA PLACA DE MANDO
 input.onButtonPressed(Button.B, function () {
     radio.sendString("defiende")
 })
+// CODIGO PARA LA PLACA CONTROL SERVOS
 let valor_Servo = 0
 servos.P0.setAngle(90)
 servos.P1.setAngle(0)
 servos.P2.setAngle(0)
 radio.setGroup(1)
+// CODIGO PARA LA PLACA DE MANDO
 basic.forever(function () {
     valor_Servo = input.acceleration(Dimension.X)
     radio.sendNumber(valor_Servo)
